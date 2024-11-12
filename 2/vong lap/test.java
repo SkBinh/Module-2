98849de9e555dfe71a67aaa9a5a7e086f2894e22
SkBinh <nlchibinh0302@gmail.com>
2024-11-12 09:39:56 +0700

    Add files via upload

---
+++ 2/vong lap/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+    int sum = 0, i = 1;
+        while (i < 10) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
+    }
+}
