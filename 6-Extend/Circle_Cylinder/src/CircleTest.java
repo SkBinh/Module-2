80f7f8a5a107cd67afa28e1e764a3449bba8e0d6
SkBinh <nlchibinh0302@gmail.com>
2024-11-18 15:31:15 +0700

    Add files via upload

---
+++ 6-Extend/Circle_Cylinder/src/CircleTest.java
@@ -0,0 +1,7 @@
+
+    public class CircleTest {
+    public static void main(String[] args) {
+    Circle circle = new Circle(3.5, "blue");
+    System.out.println(circle);
+    }
+}
---
+++ 6-Extend/Circle_Cylinder/src/CylinderTest.java
@@ -0,0 +1,6 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(3.8, "green", 8);
+        System.out.println(cylinder);
+    }
+}
