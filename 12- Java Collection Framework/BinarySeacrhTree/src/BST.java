c60f416eb74582de42ce3c92075cbbd5ea01d5b2
SkBinh <nlchibinh0302@gmail.com>
2024-11-29 10:43:57 +0700

    Add files via upload

---
+++ 12- Java Collection Framework/BinarySeacrhTree/src/BST.java
@@ -0,0 +1,58 @@
+public class BST<E extends Comparable<E>> extends AbstractTree<E> {
+    protected TreeNode<E> root;
+    protected int size = 0;
+
+    public BST() {
+    }
+
+    public BST(E[] objects) {
+        for (int i = 0; i < objects.length; i++)
+            insert(objects[i]);
+    }
+    @Override
+    public boolean insert(E e) {
+        if (root == null)
+            root = createNewNode(e);
+        else {
+            TreeNode<E> current = root;
+            TreeNode<E> parent = null;
+            while (current != null) {
+                if (e.compareTo(current.element) < 0) {
+                    parent = current;
+                    current = current.left;
+                } else if (e.compareTo(current.element) > 0) {
+                    parent = current;
+                    current = current.right;
+                } else
+                    return false;
+            }
+            if (e.compareTo(parent.element) < 0)
+                parent.left = createNewNode(e);
+            else
+                parent.right = createNewNode(e);
+        }
+        size++;
+        return true;
+    }
+
+    private TreeNode<E> createNewNode(E e) {
+        return new TreeNode<>(e);
+    }
+
+    @Override
+    public void inorder() {
+        inorder(root);
+    }
+
+    private void inorder(TreeNode<E> root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.println(root.element + " ");
+        inorder(root.right);
+    }
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+}
